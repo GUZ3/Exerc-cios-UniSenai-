e4fe6d33164d4914b189902b23ed69cb3edceaea
GUZ3 <gustavozella21@gmail.com>
2025-08-14 20:13:47 -0300

    Termino dos desafios

--- javascript/desafios/desafio3/js/validaProc.js
+++ javascript/desafios/desafio3/js/validaProc.js
@@ -1,39 +1,53 @@
 function validaProc(){
 
-    let contador=0,feminino=0,masculino=0,totalSalario=0
-    let mediaGrupo,mediaFeminino,mediaMasculino,mediaIdade
+    let contador=0,feminino=0,masculino=0,totalSalario=0,totalIdade=0,salarioFeminino=0,salarioMasculino=0
+    let mediaGrupo,mediaFeminino,mediaMasculino,mediaIdade,finalizar="",salario
 
     do{
         contador++
 
-        idade = parseInt(prompt("Insira a sua idade: "));
-        sexo = parseInt(prompt("Qual o seu gênero?\n1- Masculino\n2- Feminino"));
-        salario= parseFloat(prompt("Insira o seu salário: "));
+            idade = parseInt(prompt("Insira a sua idade: "));
+                console.log("Idade: "+idade+" anos");          
+
+            sexo = parseInt(prompt("Qual o seu gênero?\n1- Masculino\n2- Feminino"));     
+
+            salario= parseFloat(prompt("Insira o seu salário: "));
+                console.log("Salario: R$"+salario+" reais");
+
+
+            if(sexo==1){
+                masculino++
+                salarioMasculino+=salario
+
+                console.log("sexo: "+sexo+"- Masculino");
+            } else if(sexo==2){
+                feminino++
+                salarioFeminino+=salario
+
+                console.log("sexo: "+sexo+"- Feminino");
+            } else {
+                alert("Opção inválida!");
+                console.log("Opção inválida");
+            }
 
         totalSalario+=salario
         totalIdade+=idade
 
-            if(sexo == 1){
-                feminino++
+        finalizar = prompt("Digite 'finalizar' para encerrar ou pressione ENTER para continuar:");
 
-                salarioFeminino+=salario
-            } else if(sexo == 2){
-                masculino++
-
-                salarioMasculino+=salario
-            } else{
-                alert("Opção inválida!");
-                console.log("Opção inválida");
-            }
-        
-
-    } while(finalizar=="finalizar") 
+    } while(finalizar!=="finalizar") 
 
         mediaGrupo = totalSalario / contador
-        mediaFeminino = salarioFeminio / feminino
-        mediaMasculino = salarioMasculino / masculino
-        mediaIdade = totalIdade / idade
-    
-    console.log("Media do grupo: "+mediaGrupo+" media feminino: "+mediaFeminino+" media masculina: "+mediaMasculino+" media de idade: "+mediaIdade);
-    alert("Média de salário do grupo: "+mediaGrupo+" Média de salário feminino: "+mediaFeminino+" Média de salário masculina: "+mediaMasculino+" Média de idade: "+mediaIdade);
+        mediaIdade = totalIdade / contador
+
+            if(salarioFeminino>0){
+                mediaFeminino = salarioFeminino / feminino
+            }
+
+            if(salarioMasculino>0){
+                mediaMasculino = salarioMasculino / masculino
+            }
+
+    console.log("Média de salário do grupo: "+mediaGrupo+"\nMédia de salário feminino: "+mediaFeminino+"\nMédia de salário masculina: "+mediaMasculino+"\nMédia de idade: "+mediaIdade);
+    alert("Média de salário do grupo: "+mediaGrupo+"\nMédia de salário feminino: "+mediaFeminino+"\nMédia de salário masculina: "+mediaMasculino+"\nMédia de idade: "+mediaIdade);
 }
